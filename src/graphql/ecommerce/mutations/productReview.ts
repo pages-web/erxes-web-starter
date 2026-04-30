37ba9bc857f188ab781f6be5ff624de57ceaf4c6
Sodoo615 <dev@erxes.io>
2026-04-30 15:54:06 +0800

    add ecommerce graphql queries and mutations

---
+++ src/graphql/ecommerce/mutations/productReview.ts
@@ -0,0 +1,88 @@
+import { gql } from "@apollo/client";
+import type { ProductReview } from "../queries/productReview";
+
+export type ProductReviewInput = {
+  productId?: string;
+  customerId?: string;
+  review?: number;
+  description?: string;
+  info?: Record<string, unknown>;
+};
+
+export const CP_PRODUCT_REVIEW_ADD = gql`
+  mutation cpProductReviewAdd(
+    $productId: String
+    $customerId: String
+    $review: Float
+    $description: String
+    $info: JSON
+  ) {
+    cpProductReviewAdd(
+      productId: $productId
+      customerId: $customerId
+      review: $review
+      description: $description
+      info: $info
+    ) {
+      _id
+      productId
+      customerId
+      review
+      description
+      info
+    }
+  }
+`;
+
+export type CpProductReviewAddVariables = ProductReviewInput;
+
+export type CpProductReviewAddData = {
+  cpProductReviewAdd: ProductReview;
+};
+
+export const PRODUCT_REVIEW_UPDATE = gql`
+  mutation productreviewUpdate(
+    $_id: String!
+    $productId: String
+    $customerId: String
+    $review: Float
+    $description: String
+    $info: JSON
+  ) {
+    productreviewUpdate(
+      _id: $_id
+      productId: $productId
+      customerId: $customerId
+      review: $review
+      description: $description
+      info: $info
+    ) {
+      _id
+      productId
+      customerId
+      review
+      description
+      info
+    }
+  }
+`;
+
+export type ProductReviewUpdateVariables = { _id: string } & ProductReviewInput;
+
+export type ProductReviewUpdateData = {
+  productreviewUpdate: ProductReview;
+};
+
+export const PRODUCT_REVIEW_REMOVE = gql`
+  mutation productReviewRemove($_id: String!) {
+    productReviewRemove(_id: $_id)
+  }
+`;
+
+export type ProductReviewRemoveVariables = {
+  _id: string;
+};
+
+export type ProductReviewRemoveData = {
+  productReviewRemove: boolean;
+};
